1ab0b341fed3fac34b7efd18a907090c6453fb78
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-05-14 11:30:03 -0500

    SQL

--- example/changelogs/changelog.oracle.sql
+++ example/changelogs/changelog.oracle.sql
@@ -1,25 +1,16 @@
 --changeset Johan:9
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
---
--- Host: localhost    Database: pica
--- ------------------------------------------------------
--- Server version	8.0.28
-
---
--- Table structure for table `address`
---
-CREATE TABLE `address` (
-  `id` int NOT NULL,
-  `address` varchar(500) NOT NULL,
-  `status` int NOT NULL,
-  `cityCode` int NOT NULL,
-  `stateCode` int NOT NULL,
-  `userId` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_city_address_idx` (`cityCode`),
-  KEY `FK_state_address_idx` (`stateCode`),
-  KEY `FK_user_address_idx` (`userId`),
-  CONSTRAINT `FK_city_address` FOREIGN KEY (`cityCode`) REFERENCES `city` (`id`),
-  CONSTRAINT `FK_state_address` FOREIGN KEY (`stateCode`) REFERENCES `state` (`id`),
-  CONSTRAINT `FK_user_address` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+CREATE TABLE address (
+  id int NOT NULL,
+  address varchar(500) NOT NULL,
+  status int NOT NULL,
+  cityCode int NOT NULL,
+  stateCode int NOT NULL,
+  userId int NOT NULL,
+  PRIMARY KEY (id),
+  KEY FK_city_address_idx (cityCode),
+  KEY FK_state_address_idx (stateCode),
+  KEY FK_user_address_idx (userId),
+  CONSTRAINT FK_city_address FOREIGN KEY (cityCode) REFERENCES city (id),
+  CONSTRAINT FK_state_address FOREIGN KEY (stateCode) REFERENCES state (id),
+  CONSTRAINT FK_user_address FOREIGN KEY (userId) REFERENCES user (id)
 ) 
