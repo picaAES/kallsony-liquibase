6a25ca85fc48528fa7826b6793714a6003965233
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-05-14 11:46:09 -0500

    SQL

--- example/changelogs/changelog.oracle.sql
+++ example/changelogs/changelog.oracle.sql
@@ -1,9 +1,10 @@
 --changeset Johan:9
 CREATE TABLE address (
   id int NOT NULL,
-  address varchar(500) NOT NULL,
+  address varchar(80) NOT NULL,
   status int NOT NULL,
   cityCode int NOT NULL,
   stateCode int NOT NULL,
   userId int NOT NULL
 ) 
+
