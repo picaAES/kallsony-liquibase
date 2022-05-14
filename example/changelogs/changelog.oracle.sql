9dcefb5b85941cf81df94ba25e61108e85d1c3a1
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-05-14 11:52:12 -0500

    Update changelog.oracle.sql

--- example/changelogs/changelog.oracle.sql
+++ example/changelogs/changelog.oracle.sql
@@ -1,8 +1,8 @@
 --changeset Johan:9
 CREATE TABLE `address` (
   `id` int NOT NULL,
-  `address` varchar(500) NOT NULL,
-  `status` int NOT NULL,
+  --`address` varchar(500) NOT NULL,
+  --`status` int NOT NULL,
   `cityCode` int NOT NULL,
   `stateCode` int NOT NULL,
   `userId` int NOT NULL,
