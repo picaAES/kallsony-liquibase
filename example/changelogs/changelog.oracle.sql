70d9b5356e888e045cbaf3f3b6cba6c61a9c1d15
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-05-14 11:50:53 -0500

    SQL

--- example/changelogs/changelog.oracle.sql
+++ example/changelogs/changelog.oracle.sql
@@ -1,10 +1,16 @@
 --changeset Johan:9
-CREATE TABLE address (
-  id int,
-  address varchar(80),
-  status int NOT NULL,
-  cityCode int NOT NULL,
-  stateCode int NOT NULL,
-  userId int NOT NULL
-) 
-
+CREATE TABLE `address` (
+  `id` int NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `status` int NOT NULL,
+  `cityCode` int NOT NULL,
+  `stateCode` int NOT NULL,
+  `userId` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_city_address_idx` (`cityCode`),
+  KEY `FK_state_address_idx` (`stateCode`),
+  KEY `FK_user_address_idx` (`userId`),
+  CONSTRAINT `FK_city_address` FOREIGN KEY (`cityCode`) REFERENCES `city` (`id`),
+  CONSTRAINT `FK_state_address` FOREIGN KEY (`stateCode`) REFERENCES `state` (`id`),
+  CONSTRAINT `FK_user_address` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+)
