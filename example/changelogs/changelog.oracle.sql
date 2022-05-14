d792e7a8f5f3a442929d9b48b6b1c6a9d163939a
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-05-14 11:33:26 -0500

    SQL

--- example/changelogs/changelog.oracle.sql
+++ example/changelogs/changelog.oracle.sql
@@ -5,12 +5,5 @@ CREATE TABLE address (
   status int NOT NULL,
   cityCode int NOT NULL,
   stateCode int NOT NULL,
-  userId int NOT NULL,
-  PRIMARY KEY (id),
-  KEY FK_city_address_idx (cityCode),
-  KEY FK_state_address_idx (stateCode),
-  KEY FK_user_address_idx (userId),
-  CONSTRAINT FK_city_address FOREIGN KEY (cityCode) REFERENCES city (id),
-  CONSTRAINT FK_state_address FOREIGN KEY (stateCode) REFERENCES state (id),
-  CONSTRAINT FK_user_address FOREIGN KEY (userId) REFERENCES user (id)
+  userId int NOT NULL
 ) ;
